9e232fedc5c2bcaef300b9b04c00b1e494eddb80
Oladipo Oluwatoni Paul <oladipotoni@gmail.com>
2026-06-12 20:27:14 +0100

    Add SQL practice queries to notes.sql

--- basic_sql/notes.sql
+++ basic_sql/notes.sql
@@ -1,1 +1,26 @@
 
+-- BASIC SQL PRACTICE
+
+
+-- 1. View all customers
+SELECT * FROM customers;
+
+-- 2. View all products
+SELECT * FROM products;
+
+-- 3. View all orders
+SELECT * FROM orders;
+
+-- 4. Select specific columns
+SELECT CustomerName, Region
+FROM customers;
+
+-- 5. Filter data (WHERE)
+SELECT *
+FROM customers
+WHERE Region = 'West';
+
+-- 6. Sort data (ORDER BY)
+SELECT *
+FROM products
+ORDER BY Price DESC;

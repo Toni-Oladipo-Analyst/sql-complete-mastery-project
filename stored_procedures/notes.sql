c0aa4c3fff78f3ce856d4f46df8f0abf208f8876
Oladipo Oluwatoni Paul <oladipotoni@gmail.com>
2026-06-12 20:40:33 +0100

    Create stored procedures for product retrieval and revenue
    Added stored procedures to retrieve products and calculate total revenue.

--- stored_procedures/notes.sql
+++ stored_procedures/notes.sql
@@ -1,1 +1,36 @@
+DELIMITER $$
+
+CREATE PROCEDURE GetAllProducts()
+BEGIN
+    SELECT * FROM products;
+END $$
+
+DELIMITER ;
+CALL GetAllProducts();
+
+DELIMITER $$
+
+CREATE PROCEDURE GetProductsByCategory(IN cat VARCHAR(50))
+BEGIN
+    SELECT *
+    FROM products
+    WHERE Category = cat;
+END $$
+
+DELIMITER ;
+CALL GetProductsByCategory('Electronics');
+
+
+
+DELIMITER $$
+
+CREATE PROCEDURE TotalRevenue()
+BEGIN
+    SELECT SUM(Quantity * UnitPrice) AS revenue
+    FROM sales_data;
+END $$
+
+DELIMITER ;
+
+
 
